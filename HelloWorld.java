5f0a41428c0cc2a950f96042bdb3555091c8aacf
Mani091087 <msmanisandeep@gmail.com>
2023-03-01 11:03:22 +0530

    Jenkins Job

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("My first Application");	
+        System.out.println("My first Application");
+	System.out.println("Jenkins Job");	
     }
 }
